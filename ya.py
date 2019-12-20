9aca967c4fca49b7c284b4ceab873d2c6b879a51
Seedbank0727 <59091027+Seedbank0727@users.noreply.github.com>
2019-12-20 23:09:05 +0900

    Add files via upload

---
+++ ya.py
@@ -0,0 +1,37 @@
+import asyncio
+import discord
+
+app = discord.Client()
+
+token = "NTc4NTQxODc1MzM4ODA1Mjcw.XfylRg.WpwbIQqpcphF1HEJ4ZbroeDZDZc"
+
+@app.event
+async def on_ready():
+    print("다음으로 로그인합니다 : ")
+    print(app.user.name)
+    print(app.user.id)
+    print("============")
+
+    await app.change_presence(game=discord.Game(name="하나둘셋", type=1))
+
+@app.event
+async def on_message(message):
+    
+    if message.author.bot:
+        return None
+    elif message.content == "콤보시작!":
+        await app.send_message(message.channel,"하나!")
+        msg = await app.wait_for_message(timeout=8.0,author=message.author)
+        if msg is not "둘!":
+            await app.send_message(message.channel,"눈치없냐?")
+            return
+        else:
+            await app.send_message(message.channel,"셋!")
+
+    if message.content == "!미간":
+        await app.send_message(message.channel,"이거슨 제 미간이 아니라 찌찌입니다.")
+
+    if message.content == "!김나":
+        await app.send_message(message.channel,"성방!")
+
+app.run(token)
